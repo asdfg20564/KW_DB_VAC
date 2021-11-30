d9571dec3f8cf7a2b8cb54a90f52ad8c7c4e7cdf
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2021-12-01 00:20:49 +0900

    Impl: login using bcrypt password compare and more session info added

--- webapps/routes/login.js
+++ webapps/routes/login.js
@@ -1,62 +1,56 @@
 var express = require('express');
 var router = express.Router();
 const getSqlConnection = require('../configs/mysql_load');
-var session = require("express-session");
-
-
+var bcrypt = require('bcrypt');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  if(req.session.loggedin === undefined)
+router.get('/', function (req, res, next) {
+  if (req.session.loggedin === undefined)
     res.render('login', { title: '로그인' });
-  else{
+  else {
     res.send("<script>alert('이미 로그인 되었습니다.');location.href='/';</script>");
   }
-    
-  
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   var username = req.body.username;   //뒤의 username은 form에서의 name
   var passwd = req.body.passwd;
 
-  console.log(username);
-  console.log(passwd);
+  if (req.session.loggedin === 1) {
+    res.send("<script>alert('잘못된 경로로 접근했습니다!');</script>");
+  }
+  else {
 
-  getSqlConnection((conn) => {
-    //SQL 준비 작업
-    var sqlLogin = "SELECT count(*) as loggedin from USER where username = ? and passwd = ?;"//prepared statement
-
-    conn.query(sqlLogin, [username, passwd], function(err, rows){
-      //SQL 실행
-      if(err) console.err("Error: MySQL returned ERROR : " + err);
-      console.log(rows[0]);
-      //console.log(req.session);
-
-      if(req.session.loggedin === undefined)
-      {
-        if(rows[0].loggedin == 0)
-        {
-          res.send("<script>alert('실패');location.href='login';</script>");
-          //redirect가 혼자 쓰이는게 아닌 이상, 페이지 이동도 자바스크립트로 처리
+    getSqlConnection((conn) => {
+      //SQL 준비 작업
+      var sqlGetPwdofUser = "SELECT uid, legal_name, passwd from USER where username = ?;";
+      conn.query(sqlGetPwdofUser, [username], function (err, rows) {
+        if (err) console.err("Error: MySQL returned ERROR : " + err);
+        else {
+          //Compare user password with saved password
+          bcrypt.compare(passwd, rows[0].passwd, (err, isMatched) => {
+            if (err) console.err("Error: bcrypt returned ERROR : " + err);//bcrypt error
+            else {
+              if (isMatched) {//password match
+                /* NOTE: 자주 쓰는 정보는 Session에 담아두기, 매번 Query 요청은 성능 저하 */
+                req.session.loggedin = 1;//then login
+                req.session.uid = rows[0].uid;//set uid to use later
+                req.session.legal_name = rows[0].legal_name;//set name to use later
+                res.redirect("/");
+              }
+              else {//otherwise
+                res.send("<script>alert('로그인에 실패했습니다.');location.href='login';</script>");//re-login
+              }
+            }
+          })
         }
-        else
-        {
-          req.session.loggedin = 1;
-          res.redirect("/");
-        }
-      }
-      else
-      {
-        res.send("<script>alert('잘못된 경로로 접근했습니다!');</script>");
-      }
+      })
 
+      //Release connection
+      conn.release();
     });
-    //정리
-    conn.release();
-  })  
-  
-});
-  
+  }
+})
+
 module.exports = router;
-  
+
