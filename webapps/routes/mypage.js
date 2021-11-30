3af282999523f54500dd9191e27debd43ebf7a1d
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2021-12-01 00:34:51 +0900

    Impl: Mypage basic personal info displaying

--- webapps/routes/mypage.js
+++ webapps/routes/mypage.js
@@ -6,8 +6,33 @@ const getSqlConnection = require('../configs/mysql_load');
 router.get('/', function(req, res, next) {
   if(req.session.loggedin === 1)//check login
   {
-    
-    res.render('mypage', { title: '내 정보 보기', loggedin: 1, legal_name: req.session.legal_name});
+    getSqlConnection((conn) => {
+      var sqlGetUserInfo = "SELECT sex, birthdate, address, email, phone from USER where uid = ?;";
+      conn.query(sqlGetUserInfo, [req.session.uid], function (err, rows) {
+        if (err) console.err("Error: MySQL returned ERROR : " + err);
+        else{
+          var bdate = new Date(rows[0].birthdate);
+
+          var renderInfo = {
+            title: '내 정보 보기',
+            loggedin: 1,
+            legal_name: req.session.legal_name,
+            sex: (rows[0].sex === 1)?"여자":"남자",
+            byear:bdate.getFullYear(),
+            bmonth:bdate.getMonth().toString().padStart(2,"0"),
+            bday:bdate.getDay().toString().padStart(2,"0"),
+            phone: rows[0].phone,
+            email: rows[0].email,
+            address: rows[0].address
+          };
+
+          res.render('mypage', renderInfo);
+        }
+      })
+
+      //Release connection
+      conn.release();
+    })
   }
   else
   {
