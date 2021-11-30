6ced3bdcedffc08bafb934b7fef7dec925a89298
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2021-11-30 22:45:38 +0900

    typo and bugfix: true is not 1

--- webapps/routes/vacc_stat.js
+++ webapps/routes/vacc_stat.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('vacc_stat', { title: 'Express' , loggedin: (req.session.loggedin === 1)});
+  res.render('vacc_stat', { title: '백신 접종 통계 보기' , loggedin: +(req.session.loggedin === 1)});
 });
 
 module.exports = router;
