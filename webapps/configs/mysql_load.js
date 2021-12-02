11ab43a10c1f97277e6112122172567b97850a25
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2021-12-02 23:33:15 +0900

    Impl: Mysql2 promise

--- webapps/configs/mysql_load.js
+++ webapps/configs/mysql_load.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var mysql2 = require('mysql2');
+
 var mysqlConfig = require('./mysql_config.json');
 
 var pool = mysql.createPool(mysqlConfig);
@@ -11,4 +13,14 @@ function getSqlConnection(callback){
     });
 }
 
+var async_pool = mysql2.createPool(mysqlConfig);
+function getSqlConnectionAsync(callback){
+    async_pool.getConnection(function (err, conn){
+        if(!err){
+            callback(conn);
+        }
+    });
+}
+
 module.exports = getSqlConnection;
+module.exports = getSqlConnectionAsync;
