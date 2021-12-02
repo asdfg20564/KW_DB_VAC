a9bd5e56e56f2e8fdcae071f663e5cf88df78d4d
asdfg20564 <44521546+asdfg20564@users.noreply.github.com>
2021-12-02 23:41:29 +0900

    vacc_recommend update
    update vacc_recommend

--- webapps/routes/reserve_select_date.js
+++ webapps/routes/reserve_select_date.js
@@ -3,10 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  if(req.session.loggedin === undefined  || req.session.loggedin ===0)
+  if(req.session.loggedin === undefined  || req.session.loggedin ===0){
     res.send("<script>alert('로그인이 필요합니다.');location.href='login';</script>");
-  else
-    res.render('reserve_select_date', { title: '예약 날짜 선택', loggedin: 1, legal_name: req.session.legal_name});
+    var happy= req.query.body.hospital_name;
+  }
+  else{
+    var hospital_name = req.body.hospital_name;
+
+    var renderInfo = {
+      title: '예약 날짜 선택',
+      loggedin: 1,
+      hospital_name
+    };
+
+    res.render('reserve_select_date', renderInfo);
+  }
 });
 
 module.exports = router;
