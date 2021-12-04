028cc50ab9aa73a225b973fd2c02c7bce6d37827
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2021-12-04 14:14:23 +0900

    Impl: Join validation

--- webapps/routes/api/api_router.js
+++ webapps/routes/api/api_router.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var get_hospital_list_by_area = require('./get_hospital_list_by_area');
+var validate_join = require('./validate_join');
 
 router.use('/get_hospital_list_by_area', get_hospital_list_by_area);
+router.use('/validate_join', validate_join);
 
 module.exports = router;
