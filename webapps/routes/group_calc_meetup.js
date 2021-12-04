197817b7884e6f860fefd848e95791e4345a618d
heejin1998 <bobae1998@naver.com>
2021-12-04 16:55:53 +0900

    Update group_calc_meetup js

--- webapps/routes/group_calc_meetup.js
+++ webapps/routes/group_calc_meetup.js
@@ -1,18 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const getSqlConnectionAsync = require('../configs/mysql_load').getSqlConnectionAsync;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   if(req.session.loggedin === undefined || req.session.loggedin ===0)
     res.send("<script>alert('로그인이 필요합니다.');location.href='login';</script>");
   else
-    res.render('group_calc_meetup', { title: '모임 계산기', loggedin: 1, legal_name: req.session.legal_name});
+  {
+    // TEAM에서 owner_uid = req.session.uid인 사람들의 id(gid), team_name 갖고오기
+    // TEAM_MEM에서
+    var sqlAllTeamMem = "SELECT team_name, gid, legal_name, uid FROM;";
+    
+    try{
+      var conn = await getSqlConnectionAsync();
+      var renderInfo = {
+        title: '모임 계산기',
+        loggedin: 1,
+        legal_name: req.session.legal_name
+
+
+      }
+      conn.release();
+      res.render('group_calc_meetup', { title: '모임 계산기', loggedin: 1, legal_name: req.session.legal_name});
+    }catch(err){
+      console.log("Error: MySQL returned ERROR :" + err);
+      conn.release();
+    }
+    
+  }
+    
   
 });
 
 /* POST home page. */
 /* result1, result2 조건문으로 나눠줘야 함 */
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
   if(req.session.loggedin === undefined || req.session.loggedin ===0)
     res.send("<script>alert('로그인이 필요합니다.');location.href='login';</script>");
   else
